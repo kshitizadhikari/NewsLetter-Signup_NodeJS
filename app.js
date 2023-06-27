a0fc16b443aa05202b0f3e5857d13e31ed094f6f
seaweed78 <adhikarikshitiz12@gmail.com>
2023-06-27 10:19:49 +0545

    get user data

--- app.js
+++ app.js
@@ -11,6 +11,15 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/signup.html");
 });
 
+app.post("/", function (req, res) {
+  let firstName = req.body.firstName;
+  let lastName = req.body.lastName;
+  const email = req.body.email;
+
+  console.log(firstName + lastName + " " + email);
+  res.send("received");
+});
+
 app.listen(3000, function () {
   console.log("Server is running on port 3000");
 });
